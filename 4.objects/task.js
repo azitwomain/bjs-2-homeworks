139e62a93352e4f07b3476cfc2d21765d24b4218
Arkhipov <azitwomain@yandex.ru>
2024-11-11 17:53:14 +0300

    задание готово Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+    }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
+    if (this.marks) {
+        this.marks = marks
+    } else {
+        delete this.marks;
+    }
+    }
   
-}
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks) {
+    this.marks.reduce(((el, num) => el + num), 0)
+  } else {
+    return 0
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+delete this.subject
+delete this.marks
+this.exclude = reason;
 }
