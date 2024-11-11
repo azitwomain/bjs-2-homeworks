2707efcfbb5b064ddd8ac5c4e174398f994a263b
Arkhipov <azitwomain@yandex.ru>
2024-11-11 17:17:30 +0300

    задание готово, 3 задание все равно не пониаю как делать

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,9 +1,8 @@
 function getArrayParams(...arr) {
-	let numbers = [...arr];
-	let min = Math.min(...numbers);
-	let max = Math.max(...numbers);
-	let avg = numbers.reduce((sum, elem) => sum + elem) / numbers.length;
-	avg = Number(avg.toFixed(2));
+	
+	let min = Math.min(...arr);
+	let max = Math.max(...arr);
+	let avg = Number((numbers.reduce((sum, elem) => sum + elem, 0) / numbers.length).toFixed(2));
 	return {
 		min: min,
 		max: max,
@@ -12,62 +11,60 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-	if (arguments.length === 0) {
+	if (arr.length === 0) {
 		return 0;
 	}
-	let numbers = [...arr];
-	let sum = numbers.reduce((sum, elem) => sum + elem);
-	return sum;
-}
+	
+	return sum = arr.reduce((sum, elem) => sum + elem, 0);
+	}
 
 function differenceMaxMinWorker(...arr) {
 	if (arguments.length === 0) {
 		return 0;
 	}
-	let numbers = [...arr];
-	let difference = Math.max(...numbers) - Math.min(...numbers);
-	return difference;
+	return difference = Math.max(...arr) - Math.min(...arr);
+	
 }
 
 function differenceEvenOddWorker(...arr) {
 	if (arguments.length === 0) {
 		return 0;
 	}
-	let numbers = [...arr];
 	let evenNum = 0;
 	let oddNum = 0;
-	for (let i = 0; i <= numbers.length; i++) {
-		if (numbers[i] % 2 == 0) {
-			evenNum = evenNum + numbers[i]
+	for (let i = 0; i <= arr.length - 1; i++) {
+		if (arr[i] % 2 == 0) {
+			evenNum = evenNum + arr[i]
 		} else {
-			oddNum = oddNum + numbers[i]
+			oddNum = oddNum + arr[i]
 		}
 	}
-	let differenceEvenOdd = evenNum - oddNum;
-	return differenceEvenOdd;
+	return differenceEvenOdd = evenNum - oddNum;
+	
 }
 
 function averageEvenElementsWorker(...arr) {
 	if (arguments.length === 0) {
 		return 0;
 	}
-	let numbers = [...arr];
+	
 	let evenNum = 0;
 	let evenCount = 0;
-	for (let i = 0; i <= numbers.length; i++) {
+	for (let i = 0; i <= arr.length - 1; i++) {
 		if (numbers[i] % 2 == 0) {
-			evenNum = evenNum + numbers[i]
+			evenNum = evenNum + arr[i]
 			evenCount++
 		}
 	}
-	let averageEven = evenNum / evenCount;
-	return averageEven;
+	return averageEven = evenNum / evenCount;
+	
 }
 
-function makeWork(arrOfArr, func) {
+function makeWork(arrOfArr, func) {	
 	let maxWorkerResult = -Infinity;
-	for (let i = 0; i <= arrOfArr.length; i++) {
-
+	for (let i = 0; i <= arrOfArr.length - 1; i++) {
+		funk.push(...arrOfArr[i])
+		
 	}
 	return func
 
