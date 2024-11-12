b757d0450fcbdf0b7e8470edd67da381f741046a
Arkhipov <azitwomain@yandex.ru>
2024-11-12 04:40:28 +0300

    Вроде бы все замечания исправил

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -10,52 +10,52 @@ class PrintEditionItem {
         return this.state = this.state * 1.5;
     }
 
-    set changeState(count) {
+    set state(count) {
         if (count < 0) {
-            this.state = 0;
+            this._state = 0;
         } else if (count > 100) {
-            this.state = 100;
+            this._state = 100;
         } else {
-            this.state = count;
+            this._state = count;
         }
     }
-    get readState() {
-        return this.state
+    get state() {
+        return this._state
     }
 
 }
 
 class Magazine extends PrintEditionItem {
-    constructor() {
-        super()
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount)
         this.type = "magazine";
     }
 }
 
 class Book extends PrintEditionItem {
-    constructor(bookAuthor) {
-        super(bookAuthor)
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount)
         this.type = "book";
-        this.author = bookAuthor;
+        this.author = author;
     }
 }
 
 class NovelBook extends Book {
-    constructor(bookAuthor) {
-        super(bookAuthor)
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount)
 
         this.type = "novel";
     }
 }
 class FantasticBook extends Book {
-    constructor(bookAuthor) {
-        super(bookAuthor)
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCountr)
         this.type = "fantastic";
     }
 }
 class DetectiveBook extends Book {
-    constructor(bookAuthor) {
-        super(bookAuthor)
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount)
 
         this.type = "detective";
     }
