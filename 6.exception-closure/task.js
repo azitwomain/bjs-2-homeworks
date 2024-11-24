968edbbe00294b598df8847d27034c419fe37611
Arkhipov <azitwomain@yandex.ru>
2024-11-24 17:37:54 +0300

    задание исправил

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,47 +1,44 @@
 ﻿function parseCount(parseCount) {
-    let count;
-    try {
-        count = Number.parseFloat(parseCount);
-    } catch (error) {
-        console.error("Невалидное значение")
+    let count = Number.parseFloat(parseCount);
+    if (count === NaN) {
+        throw new Error("Невалидное значение")
+    } else {
+        return count;
     }
-    return count;
 }
 function validateCount(parseCount) {
-    let count;
     try {
-        count = parseCount(parseCount);
+        return parseCount(parseCount);
     } catch (error) {
         return error;
     }
-    return count;
 }
 
 class Triangle {
     constructor(a, b, c) {
+        let a = a;
+        let b = b;
+        let c = c;
         this.perimeter = 0;
         this.area = 0;
-        try {
-            if (a + b < c || a + c < b || c + b < a) {
-                throw new Error("Треугольник с такими сторонами не существует")
-            }
-        } catch {
-            console.error("Треугольник с такими сторонами не существует")
+        if (a + b < c || a + c < b || c + b < a) {
+            throw new Error("Треугольник с такими сторонами не существует")
         }
+
     }
     get perimeter() {
-        return this._perimeter = a + b + c
+        return this.perimeter = a + b + c
     }
     get area() {
-        const p = (a + b + c) / 2;
-        return this._area = Number(Math.sqrt(p * (p - a) * (p - b) * (p - c)).toFixed(3));
+        const p = perimeter / 2;
+        return Number(Math.sqrt(p * (p - a) * (p - b) * (p - c)).toFixed(3));
     }
 }
 
 function getTriangle(a, b, c) {
-    const abcTriangle = new Triangle(4, 4, 4)
+
     try {
-        return abcTriangle
+        return abcTriangle = new Triangle(a, b, c)
     } catch (error) {
         abcTriangle.perimeter = "Ошибка! Треугольник не существует"
         abcTriangle.area = "Ошибка! Треугольник не существует"
