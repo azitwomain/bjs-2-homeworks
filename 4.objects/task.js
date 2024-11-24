7086123de7753a62f147d1792f8fcf889736e890
Arkhipov <azitwomain@yandex.ru>
2024-11-24 20:26:50 +0300

    задание исправил

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,33 +1,38 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
-    }
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+}
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if (this.marks) {
-        this.marks.push(marks);
-    }
-    }
-  
+  if (this.marks) {
+    this.marks.push(...marks);
+  }
+}
+
 
 Student.prototype.getAverage = function () {
-  let count;
+
   if (this.marks) {
-    count = this.marks.reduce(((el, num) => el + num), 0)
-    return avg = count / this.marks.length;
+    let sum = this.marks.reduce(((el, num) => el + num), 0)
+    if (this.marks === 0 || this.marks.length === 0 ) {
+      return 0
+    } else {
+      return sum / this.marks.length;
+    }
+   
   } else {
     return 0
   }
 }
 
-Student.prototype.exclude = function (reason) {
-delete this.subject
-delete this.marks
-this.exclude = reason;
+Student.prototype.exclude  = function (reason) {
+  delete this["subject"]
+  delete this["marks"]
+  this.excluded  = reason;
 }
