88a2597ad8de942c5ec7e98ba90b616c08fcd8c2
Arkhipov <azitwomain@yandex.ru>
2024-12-06 22:59:44 +0300

    задание исправил

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -16,30 +16,34 @@ function validateCount(parseNum) {
 
 class Triangle {
     constructor(a, b, c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
         if (a + b < c || a + c < b || c + b < a) {
             throw new Error("Треугольник с такими сторонами не существует")
         }
+        this.a = a;
+        this.b = b;
+        this.c = c;
 
     }
     get perimeter() {
-        return this.perimeter = a + b + c
+        return this.a + this.b + this.c;
     }
     get area() {
-        const p = this.perimeter / 2;
-        return Number(Math.sqrt(p * (p - a) * (p - b) * (p - c)).toFixed(3));
+        let p = this.perimeter / 2;
+        return +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3);
     }
 }
 
 function getTriangle(a, b, c) {
-
     try {
-        return abcTriangle = new Triangle(a, b, c)
-    } catch (error) {
-        abcTriangle.perimeter = "Ошибка! Треугольник не существует"
-        abcTriangle.area = "Ошибка! Треугольник не существует"
-        return abcTriangle
+      return new Triangle(a, b, c);
+    } catch(error) {
+        return {
+          get perimeter() {
+            return `Ошибка! Треугольник не существует`;
+          },
+          get area() {
+            return `Ошибка! Треугольник не существует`;
+          }
+        }
     }
-}
+  }
